cda161fa11d44e3389a8db55f09db66616418535
Chamu203 <104495714+Chamu203@users.noreply.github.com>
2022-05-10 07:15:28 +0530

    first commit

--- task_28_04_2022/singlefact.c
+++ task_28_04_2022/singlefact.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
-//to find the factors of number
+// to find the factors of number
 void find_factors(int number)
 {
     for (int index = 1; index <= number / 2; index++)
     {
         if (number % index == 0)
-        // printing the  factors of number
+            // printing the  factors of number
             printf("%d\n", index);
     }
 }
-//printing function
+// printing function
 void print()
 {
-    int index,number;
-    if(number%index==0)
-        printf("factors of number are:%d\n",index);
+    int index, number;
+    if (number % index == 0)
+        printf("factors of number are:%d\n", index);
     else
         printf("no factors founds");
 }
 int main()
 {
     int number = 10;
-// function calling
+    // function calling
     find_factors(number);
     print();
     return 0;
