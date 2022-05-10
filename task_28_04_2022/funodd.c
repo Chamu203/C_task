f8bea03cac0e367316e681f4a95162f0db3aa44a
Chamu203 <104495714+Chamu203@users.noreply.github.com>
2022-05-10 07:16:43 +0530

    first commit

--- task_28_04_2022/funodd.c
+++ task_28_04_2022/funodd.c
@@ -1,19 +1,21 @@
-#include<stdio.h>
-void odd_number(int minimum,int maximum)
+#include <stdio.h>
+void odd_number(int minimum, int maximum)
 {
-    int array[maximum-minimum];
-    for(int i=0,j=minimum;i<(maximum-minimum)&&j<maximum;i++,j++)
-    array[i]=j;
+    int array[maximum - minimum];
+    for (int i = 0, j = minimum; i < (maximum - minimum) && j < maximum; i++, j++)
+        array[i] = j;
     printf("Odd numbers\n");
-    for(int i=0;i<(maximum-minimum);i++){
-        if(array[i]%2!=0){
-            printf("odd numbers:%d\n",array[i]);
+    for (int i = 0; i < (maximum - minimum); i++)
+    {
+        if (array[i] % 2 != 0)
+        {
+            printf("odd numbers:%d\n", array[i]);
             i++;
         }
     }
 }
 int main()
 {
-    int minimum=1,maximum=20;
-    odd_number(minimum,maximum);
+    int minimum = 1, maximum = 20;
+    odd_number(minimum, maximum);
 }
