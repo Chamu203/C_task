20327f49a1eab4b120247fb50e1ae9490e930bfd
Chamu203 <104495714+Chamu203@users.noreply.github.com>
2022-05-10 07:18:05 +0530

    first commit

--- task_28_04_2022/funeven.c
+++ task_28_04_2022/funeven.c
@@ -1,19 +1,21 @@
-#include<stdio.h>
-void even_number(int minimum,int maximum)
+#include <stdio.h>
+void even_number(int minimum, int maximum)
 {
-    int array[maximum-minimum];
-    for(int i=0,j=minimum;i<(maximum-minimum)&&j<maximum;i++,j++)
-    array[i]=j;
+    int array[maximum - minimum];
+    for (int i = 0, j = minimum; i < (maximum - minimum) && j < maximum; i++, j++)
+        array[i] = j;
     printf("even numbers\n");
-    for(int i=0;i<(maximum-minimum);i++){
-        if(array[i]%2==0){
-            printf("even numbers:%d\n",array[i]);
+    for (int i = 0; i < (maximum - minimum); i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            printf("even numbers:%d\n", array[i]);
             i++;
         }
     }
 }
 int main()
 {
-    int minimum=1,maximum=20;
-    even_number(minimum,maximum);
+    int minimum = 1, maximum = 20;
+    even_number(minimum, maximum);
 }
